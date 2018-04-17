7af0820dd31a8bc9b48c6ec7d883fe8d2cc34c4b
jerome <j.vanbersy@comaseinfo.be>
2018-04-17 14:03:35 +0200

    module created, added min & max abilities, 24hours time format now displayed on 2 circles, every minute is now selectable within the minutes clock

--- src/time-control/w-clock/w-clock.component.ts
+++ src/time-control/w-clock/w-clock.component.ts
@@ -1,142 +1,173 @@
-
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, OnChanges} from '@angular/core';
 
 // Current type to show
 export enum CLOCK_TYPE {
-    HOURS = 1,
-    MINUTES = 2
+  HOURS = 1,
+  MINUTES = 2
 }
 
 export type TimeFormat = 12 | 24;
 
 export interface ITime {
 
-    hour: number;
-    minute: number;
-    meriden: 'PM' | 'AM';
-    format: TimeFormat;
-};
+  hour: number;
+  minute: number;
+  meriden: 'PM' | 'AM';
+  format: TimeFormat;
+}
 
 @Component({
-    selector: 'w-clock',
-    styleUrls: ['./w-clock.component.scss'],
-    templateUrl: './w-clock.component.html'
+  selector: 'w-clock',
+  styleUrls: ['./w-clock.component.scss'],
+  templateUrl: './w-clock.component.html'
 })
 export class WClockComponent implements OnChanges {
 
-    @Input() public userTime: ITime;
-    @Output() public userTimeChange: EventEmitter<ITime> = new EventEmitter();
+  @Input() public userTime: ITime;
+  @Input() public minTime: ITime;
+  @Input() public maxTime: ITime;
+  @Output() public userTimeChange: EventEmitter<ITime> = new EventEmitter();
 
-    @Input() public currentView: CLOCK_TYPE;
-    @Output() public viewChange = new EventEmitter<CLOCK_TYPE>();
+  @Input() public currentView: CLOCK_TYPE;
+  @Output() public viewChange = new EventEmitter<CLOCK_TYPE>();
 
-    @Input() public color: string;
+  @Input() public color: string;
 
-    public steps = new Array<number>();
-    private selectedTimePart;
-    private STEP_DEG: number;
+  public steps = new Array<number>();
+  private selectedTimePart;
+  private STEP_DEG: number;
 
+  ngOnChanges() {
+    this.setupUI();
+  }
 
+  private setupUI() {
 
-    ngOnChanges() {
+    this.steps = new Array<number>();
 
-        this.setupUI();
+    switch (this.currentView) {
+
+      case CLOCK_TYPE.HOURS:
+
+        for (let i = 1; i <= this.userTime.format; i++) {
+
+          this.steps.push(i);
+          this.selectedTimePart = this.userTime.hour || 0;
+
+          if (this.selectedTimePart > this.userTime.format) {
+
+            this.selectedTimePart -= this.userTime.format;
+          }
+        }
+        break;
+
+      case CLOCK_TYPE.MINUTES:
+
+        for (let i = 1; i <= 59; i += 1) {
+
+          this.steps.push(i);
+        }
+        this.steps.push(0);
+        this.selectedTimePart = this.userTime.minute || 0;
+        break;
+    }
+  }
+
+  public getPointerStyle() {
+
+    let divider = 1;
+    switch (this.currentView) {
+
+      case CLOCK_TYPE.HOURS:
+        divider = 12; // this.userTime.format;
+        break;
+
+      case CLOCK_TYPE.MINUTES:
+        divider = 60;
+        break;
     }
 
-
-    private setupUI() {
-
-        this.steps = new Array<number>();
-
-        switch (this.currentView) {
-
-            case CLOCK_TYPE.HOURS:
-
-                for (let i = 1; i <= this.userTime.format; i++) {
-
-                    this.steps.push(i);
-                    this.selectedTimePart = this.userTime.hour || 0;
-
-                    if (this.selectedTimePart > this.userTime.format) {
-
-                        this.selectedTimePart -= this.userTime.format;
-                    }
-                }
-                break;
-
-            case CLOCK_TYPE.MINUTES:
-
-                for (let i = 5; i <= 55; i += 5) {
-
-                    this.steps.push(i);
-                }
-                this.steps.push(0);
-                this.selectedTimePart = this.userTime.minute || 0;
-                break;
-        }
+    let degrees = 0;
+    if (this.currentView === CLOCK_TYPE.HOURS) {
+      degrees = Math.round(this.userTime.hour * (360 / divider)) - 180;
+    } else {
+      degrees = Math.round(this.userTime.minute * (360 / divider)) - 180;
     }
 
-    public getPointerStyle() {
-
-        let divider = 1;
-        switch (this.currentView) {
-
-            case CLOCK_TYPE.HOURS:
-                divider = this.userTime.format;
-                break;
-
-            case CLOCK_TYPE.MINUTES:
-                divider = 60;
-                break;
-        }
-
-        let degrees = 0;
-        if (this.currentView === CLOCK_TYPE.HOURS) {
-            degrees = Math.round(this.userTime.hour * (360 / divider)) - 180;
-        } else {
-            degrees = Math.round(this.userTime.minute * (360 / divider)) - 180;
-        }
-
-        const style = {
-
-            '-webkit-transform': 'rotate(' + degrees + 'deg)',
-            '-ms-transform': 'rotate(' + degrees + 'deg)',
-            'transform': 'rotate(' + degrees + 'deg)'
-        };
-
-        return style;
+    let style = {
+      '-webkit-transform': 'rotate(' + degrees + 'deg)',
+      '-ms-transform': 'rotate(' + degrees + 'deg)',
+      'transform': 'rotate(' + degrees + 'deg)'
+    };
+    if (this.currentView === CLOCK_TYPE.HOURS && this.userTime.hour <= 12) {
+      style = Object.assign(style, {height: '30%', bottom: '20%'});
     }
 
-    private getTimeValueClass(step: number, index: number) {
+    return style;
+  }
 
-        if (this.currentView === CLOCK_TYPE.HOURS) {
-            this.STEP_DEG = 360 / this.userTime.format;
-        } else {
-            this.STEP_DEG = 360 / 12;
-        }
-        let classes = 'w-clock-step w-clock-deg' + (this.STEP_DEG * (index + 1));
-
-        if (this.selectedTimePart === step) {
-
-            classes += ' mat-primary';
-        }
-
-        return classes;
+  public disabledTimeValue(step: number): boolean {
+    if (!this.minTime && !this.maxTime) {
+      return false;
     }
-
-    private changeTimeValue(step: number) {
-
-        if (this.currentView === CLOCK_TYPE.HOURS) {
-            this.userTime.hour = step;
-
-            // auto switch to minutes
-            this.viewChange.emit(CLOCK_TYPE.MINUTES);
-        } else {
-            this.userTime.minute = step;
-
-            // auto switch to hours
-            this.viewChange.emit(CLOCK_TYPE.HOURS);
-        }
-        this.userTimeChange.emit(this.userTime)
+    if (this.currentView === CLOCK_TYPE.HOURS) {
+      if (this.minTime && this.minTime.hour > step) {
+        return true;
+      }
+      return this.maxTime && this.maxTime.hour < step;
     }
+    if (this.currentView === CLOCK_TYPE.MINUTES) {
+      if ((this.minTime && this.minTime.hour > this.userTime.hour) || (this.maxTime && this.maxTime.hour < this.userTime.hour)) {
+        return true;
+      }
+      if (this.minTime && this.maxTime && this.minTime.hour === this.userTime.hour && this.maxTime.hour === this.userTime.hour) {
+        return step < this.minTime.minute || step > this.maxTime.minute;
+      }
+      if (this.minTime && this.minTime.hour === this.userTime.hour) {
+        return step < this.minTime.minute;
+      }
+      if (this.maxTime && this.maxTime.hour === this.userTime.hour) {
+        return step > this.maxTime.minute;
+      }
+      return false;
+    }
+  }
+
+  public getTimeValueClass(step: number, index: number) {
+    // const stepDeg = this.currentView === CLOCK_TYPE.HOURS ? 360 / this.userTime.format : 360 / 12;
+    const stepDeg = this.currentView === CLOCK_TYPE.HOURS ? 360 / 12 : 360 / 60;
+    // const stepDeg = this.currentView === CLOCK_TYPE.HOURS ? 360 / 12 : 360 / 12;
+    const degrees = this.currentView === CLOCK_TYPE.HOURS && step > 12 ? (stepDeg * (index + 1 - 12)) : (stepDeg * (index + 1));
+    let classes = 'w-clock-step w-clock-deg' + degrees;
+    if (this.currentView === CLOCK_TYPE.MINUTES) classes += ' smaller';
+    if (this.currentView === CLOCK_TYPE.HOURS && this.userTime.format === 24 && step <= 12) classes += ' inside';
+
+    if (this.selectedTimePart === step) {
+
+      classes += ' mat-primary';
+    }
+    return classes;
+  }
+
+  public changeTimeValue(step: number) {
+
+    if (this.currentView === CLOCK_TYPE.HOURS) {
+      this.userTime.hour = step;
+
+      // auto switch to minutes
+      this.viewChange.emit(CLOCK_TYPE.MINUTES);
+    } else {
+      this.userTime.minute = step;
+
+      // auto switch to hours
+      // this.viewChange.emit(CLOCK_TYPE.HOURS);
+    }
+    this.userTimeChange.emit(this.userTime);
+  }
+
+  public displayStep(step: number) {
+    if (this.currentView === CLOCK_TYPE.HOURS && step === 24) return '00';
+    if (this.currentView === CLOCK_TYPE.MINUTES && step % 5 !== 0) return '';
+    return step.toString();
+  }
 }
--- src/time-control/w-time-dialog/w-time-dialog.component.ts
+++ src/time-control/w-time-dialog/w-time-dialog.component.ts
@@ -14,27 +14,28 @@ import { CLOCK_TYPE, ITime } from '../w-clock/w-clock.component';
 export class WTimeDialogComponent {
 
     public userTime: ITime;
+    public maxTime: ITime;
+    public minTime: ITime;
     private VIEW_HOURS = CLOCK_TYPE.HOURS;
     private VIEW_MINUTES = CLOCK_TYPE.MINUTES;
     private currentView: CLOCK_TYPE = this.VIEW_HOURS;
 
     constructor(
-        @Inject(MAT_DIALOG_DATA) private data: { time: ITime, color: string },
+        @Inject(MAT_DIALOG_DATA) private data: { time: ITime, color: string, minTime?: ITime, maxTime?: ITime },
         @Inject(MAT_DIALOG_DATA) public color: string,
-        private dialogRef: MatDialogRef<WTimeDialogComponent>) {
-
+        private dialogRef: MatDialogRef<WTimeDialogComponent>,
+    ) {
         this.userTime = data.time;
         this.color = data.color;
-        console.log('this.color', this.color);
+        this.maxTime = data.maxTime;
+        this.minTime = data.minTime;
     }
 
     public revert() {
-
         this.dialogRef.close(-1);
     }
 
     public submit() {
-
         this.dialogRef.close(this.userTime);
     }
 }
